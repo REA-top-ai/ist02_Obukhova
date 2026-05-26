6c314af983d6d7ead058e4e1f5cf3b2564e8cc98
albina <55658155+cartophan@users.noreply.github.com>
2026-05-26 22:24:11 +0300

    Delete REA-top-ai-ist02_Obukhova directory

--- REA-top-ai-ist02_Obukhova/API_app/client/_init_.py
+++
@@ -1,9 +0,0 @@
-from .API_methods import get_top_headlines, get_everything, get_sources
-
-__all__ = [
-    'get_top_headlines',
-    'get_everything',
-    'get_sources'
-]
-
-__version__ = '1.0.0'
